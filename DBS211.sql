b9bcfa7a686e95ba838d4a679ae95d345381a787
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2022-09-21 14:51:06 -0400

    first commit

---
+++ DBS211.sql
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** 
+
+SELECT 
+customernumber,
+contactfirstname || ' ' || contactlastname AS "Full Name",
+addressline1 || ' ' || addressline2 || '. ' || city || ', ' || state || ', ' || country AS "Full Address"
+FROM customers
+WHERE UPPER(contactfirstname) LIKE 'MIKE%';
+
+FILTER
+------
+SELECT contactfirstname || ' ' || contactlastname AS "Customer Full Name"
+FROM customers
+ORDER BY contactfirstname DESC;
+
+LIMITING
+--------
+SELECT contactfirstname || ' ' || contactlastname AS "Customer Full Name"
+FROM customers
+ORDER BY contactfirstname DESC
+OFFSET 20 ROWS
+FETCH NEXT 10 ROWS ONLY;
+
+*/
