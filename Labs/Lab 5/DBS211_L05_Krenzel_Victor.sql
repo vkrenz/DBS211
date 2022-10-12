7aef5f6387928b90c651152c0e26544185c6fc44
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2022-10-12 14:13:19 -0400

    first commit

--- Labs/Lab 5/DBS211_L05_Krenzel_Victor.sql
+++ Labs/Lab 5/DBS211_L05_Krenzel_Victor.sql
@@ -1,4 +1,4 @@
--- DBS211 Assignment 1
+-- DBS211 Lab 05
 -- Victor Krenzel
 -- 102446176
 -- 10/12/2022
@@ -12,12 +12,50 @@ SET AUTOCOMMIT ON;
 --    L5_ACTORS (actorid:int, name:varchar(20), lastname:varchar(30))
 --    L5_CASTINGS (movieid:int, actorid:int)
 --    L5_DIRECTORS(id:int, name:varchar(20), lastname:varchar(30))
+DROP TABLE L5_movies;
+DROP TABLE L5_actors;
+DROP TABLE L5_castings;
+DROP TABLE L5_directors;
+
+CREATE TABLE L5_movies (
+mID         INT           PRIMARY KEY,
+title       VARCHAR(35)   NOT NULL,
+releaseYear INT           NOT NULL,
+director    INT           NOT NULL,
+score       DECIMAL(3, 2)
+);
+
+CREATE TABLE L5_actors (
+aID       INT           PRIMARY KEY,
+firstname VARCHAR(20)   NOT NULL,
+lastname  VARCHAR(30)   NOT NULL
+);
+
+CREATE TABLE L5_castings (
+movieID   INT           PRIMARY KEY,
+actorID   INT           PRIMARY KEY
+);
+
+CREATE TABLE L5_directors (
+directorID INT           PRIMARY KEY,
+firstname  VARCHAR(20)   NOT NULL,
+lastname   VARCHAR(30)   NOT NULL
+);
 
 -- 2. Modify the movies table to create a foreign key constraint that refers to table directors.
+ALTER TABLE L5_movies
+    ADD CONSTRAINT movies_directors_fk FOREIGN KEY (director) REFERENCES L5_directors(id);
 
 -- 3. Modify the movies table to create a new constraint so the uniqueness of the movie title is guaranteed.
-
+ALTER TABLE L5_movies
+    ADD CONSTRAINT title_unique UNIQUE (title);
+    
 -- 4. Write insert statements to add the following data to table directors and movies.
+INSERT ALL
+    INTO directors VALUES (1010, 'Rob', 'Minkoff', '200 Young St. N., NULL, 'ON', 'Canada', 'M4A3A1', 'NA')
+    INTO directors VALUES (9, 'Oshawa', '+1 905 555 2222', '155 Simcoe. S., NULL, 'ON', 'Canada', 'N2L3G4', 'NA')
+    INTO directors VALUES (10, 'Montreal', '+1 268 555 3333', '1245 Rue Lavac, NULL, 'QC', 'Canada', 'K3S2H4', 'NA')
+    SELECT * FROM dual;
 
 -- 5. Write SQL statements to remove all above tables. Is the order of tables important when removing? Why?
 
